ad29e3422b237fe6631eddb04dda3c1c829ad636
Jstanislaus <48177467+Jstanislaus@users.noreply.github.com>
2023-03-31 15:02:31 +0100

    Update Slideshow.py

--- Slideshow.py
+++ Slideshow.py
@@ -22,7 +22,7 @@ win.geometry(str(screen_width)+"x"+str(screen_height))
 #resize
 #######
 #find divisor
-path = "/home/pi64/PiSlideshow/Slideshow"
+path = "/home/pi64/PiSlideshow"
 #path = input("Please give the directory location of your files")
 print("Resizing photos...")
 dir_list = os.listdir(path)
